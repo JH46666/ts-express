0a9025c580ad6dc51a659a7c9100e0d42796998b
SJH <1039460820@qq.com>
2019-08-26 07:20:26 +0800

    ts版本express

---
+++ mysql/index.sh
@@ -0,0 +1,9 @@
+
+var connection = mysql.createConnection({      //创建mysql实例
+    host:'127.0.0.1',
+    port:'3306',
+    user:'root',
+    password:'shenjunhong',
+    database:'myserver'
+    
+})
